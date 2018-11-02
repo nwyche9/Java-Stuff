d435d1a12d5b41e151635b7b52b1f7f274f1b67e
Nate Wyche <nwyche9@gmail.com>
2018-11-02 09:43:35 -0400

    Add existing file

---
+++ PycharmProjects/Weights/weights_demo.py
@@ -0,0 +1,15 @@
+from sklearn import tree
+
+# height in centimeters, weight in kilograms, shoe size in centimeters
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40], [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', 'male', 'female', 'male']
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X,Y)
+
+prediction = clf.predict([[160,70,30]])
+
+print(prediction)
